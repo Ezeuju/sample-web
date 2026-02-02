dadd0186f5b75ec9d18e3d4ef1d2a85707f8ed2c
Ezeuju <ezeuju1919@gmail.com>
2026-02-02 16:40:19 +0100

    updated

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ resetBtn.addEventListener('click', () => {
     count = 0;
     counterDisplay.innerText = count;
 });
+
+
+const menuToggle = document.getElementById('mobile-menu');
+const navList = document.getElementById('nav-list');
+const navbar = document.querySelector('.navbar'); // Select the whole navbar
+
+menuToggle.addEventListener('click', () => {
+    navList.classList.toggle('active'); // Shows the menu links
+    navbar.classList.toggle('active');  // Triggers the hamburger-to-X animation
+});
